0dfea825b932b050a5ba438e7988a51b19759b1c
Zafuzi <zacharyfoutz@gmail.com>
2017-09-02 17:27:57 -0700

    FEAT: added ability to pull from MySQL database using routes

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
-// content of server.js
-const http = require('http')
-const port = 8000
+var express = require('express');
+var app = express();
 
-const requestHandler = (request, response) => {
-    console.log(request.url)
-    var date = new Date()
-    response.end('Current date and time: ' + date)
-}
+var mysql = require('mysql');
 
-const server = http.createServer(requestHandler)
+var conn = mysql.createConnection({host: 'localhost', user: 'root', password: 'Noah2014!', database: 'foobar'});
 
-server.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-    console.log(`server is listening on ${port}`)
-})
+conn.connect();
+
+// Default route
+app.get('/', function (request, response) {
+    response.send("Welcome to Foobar lookups. Try searching for an entry by appending it to the add" +
+            "ress of the page. ");
+});
+
+// Routes with extra data
+app.get('/:query', function (request, response) {
+    var q = 'SELECT value FROM foobar.lookups WHERE lookups.key="' + request.params.query + '"';
+    conn.query(q, function (error, results) {
+        if (error) {
+            response
+                .status(400)
+                .send('Error in database operation');
+            console.log(request.params.query, q)
+        } else {
+            if (results.length > 0) {
+                response.send('value=' + results[0].value);
+            } else {
+                response.send('Value for key "' + request.params.query + '" not found.')
+            }
+        }
+    });
+});
+
+app.listen(8000);
